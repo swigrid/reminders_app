1e4062e6b75082ce45e897eb6b9d266d60f07a9b
Roman Dekret <dekret.roman@icloud.com>
2025-11-20 17:44:45 +0100

    Update seed data to create 1500 reminders; improve output messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,23 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+puts 'Deleting existing reminders...'
 Reminder.destroy_all
 
-50.times do
+reminders_to_create = 1500
+
+puts "Creating #{reminders_to_create} reminders..."
+reminders_to_create.times do
   created_at = Faker::Time.backward(days: 60)
   Reminder.create!(
     title: Faker::Lorem.sentence(word_count: 3),
     description: Faker::Lorem.paragraph,
     price: Faker::Number.decimal(l_digits: 2, r_digits: 2),
-    recurrence: ["daily", "weekly", "monthly", nil].sample,
+    recurrence: ['daily', 'weekly', 'monthly', nil].sample,
     remind_at: Faker::Time.forward(days: 30),
     created_at: created_at,
     updated_at: created_at
   )
 end
 
-puts "Created 50 reminders"
+puts "Created #{Reminder.count} reminders'"
