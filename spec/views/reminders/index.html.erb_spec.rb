1c28b3f93800cac244de4e434346fb8e42697164
Roman Dekret <dekret.roman@icloud.com>
2025-11-20 13:55:34 +0100

    Refactor index view specs for reminders; improve formatting and update assertion counts

--- spec/views/reminders/index.html.erb_spec.rb
+++ spec/views/reminders/index.html.erb_spec.rb
@@ -1,30 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "reminders/index", type: :view do
-  
-
+RSpec.describe 'reminders/index', type: :view do
   before(:each) do
     create(:reminder,
-        title: "Title",
-        description: "Description",
-        price: "9.99",
-        recurrence: "monthly"
-      )
-      create(:reminder,
-        title: "Title",
-        description: "Description",
-        price: "9.99",
-        recurrence: "monthly"
-      )
+           title: 'Title',
+           description: 'Description',
+           price: '9.99',
+           recurrence: 'monthly')
+    create(:reminder,
+           title: 'Title',
+           description: 'Description',
+           price: '9.99',
+           recurrence: 'monthly')
 
+    assign(:due_reminders, Reminder.all)
     assign(:reminders, Reminder.all.page(1))
   end
 
-  it "renders a list of reminders" do
+  it 'renders a list of reminders' do
     render
-    assert_select "div.col-12", text: Regexp.new("Title".to_s), count: 2
-    assert_select "div.col-12", text: Regexp.new("Description".to_s), count: 2
-    assert_select "div.col-4", text: Regexp.new("9.99".to_s), count: 2
-    assert_select "div.col-4", text: Regexp.new("Recurrence".to_s), count: 2
+    assert_select 'div.col-12', text: Regexp.new('Title'), count: 4
+    assert_select 'div.col-12', text: Regexp.new('Description'), count: 4
+    assert_select 'div.col-4', text: Regexp.new('9.99'), count: 4
+    assert_select 'div.col-4', text: Regexp.new('Recurrence'), count: 4
   end
 end
